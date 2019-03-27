de5ed7d96a9089d1fb93d28be9de4cefa7de4f1d
IvanTsypyshev <deadline.3012@gmail.com>
2019-03-27 23:27:42 +0700

    Test7*
    Test7*

--- src/Theme3.java
+++ src/Theme3.java
@@ -27,6 +27,7 @@ public class Theme3 {
         capabilities.setCapability("automationName","Appium");
         capabilities.setCapability("appPackage","org.wikipedia");
         capabilities.setCapability("appActivity",".main.MainActivity");
+        capabilities.setCapability("orientation","PORTRAIT"); // или LANDSCAPE
 
         capabilities.setCapability("app","C:\\Users\\FBI\\IdeaProjects\\ApiumTraining\\JavaAppiumAutomation\\apks\\org.wikipedia.apk"); //Дом
         //capabilities.setCapability("app","D:\\GitHub\\JavaAppiumAutomation\\apks\\org.wikipedia.apk");  //Работа
@@ -40,77 +41,13 @@ public class Theme3 {
     public void tearDawn(){
         driver.quit();
     }
-    String search_line = "Appium";
+
 
     @Test
 
-    public void findTitleWithoutTimeOut() {
-
-
-
-        waitForElementAndClick(
-                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
-                "Can not Find Search Wikipedia",
-                5);
-
-
-
-        waitForElementAndSendKeys(
-                By.xpath("//*[contains(@text,'Search…')]"),
-                search_line,
-                "Can not Find search input",
-                5);
-
-        waitForElementAndClick(
-                By.xpath("//*[@resource-id = 'org.wikipedia:id/page_list_item_container']//*[@text='Appium']"),
-                "Can not Find Search Wikipedia topic searching by" + search_line,
-                15);
-
-
-        String attribForSearch  = searchTitleWithoutTimeout(
-                By.id("org.wikipedia:id/view_page_title_text"),
-                "text",
-                "Cannot find title of article"
-        );
-
-        Assert.assertEquals(
-                "Article title not equals the "+search_line+" title",
-                search_line,
-                attribForSearch
-
-        );
-
+    public void someTest() {
 
     }
 
-
-
-
-    private WebElement waitForElementPresent(By by, String error_message, long timeoutInSecond){
-        WebDriverWait wait = new WebDriverWait(driver,timeoutInSecond);
-        wait.withMessage(error_message + "\n");
-        return wait.until(ExpectedConditions.presenceOfElementLocated(by));
-
-    }
-
-    private WebElement waitForElementAndClick(By by, String error_message, long timeoutInSecond){
-
-        WebElement element = waitForElementPresent(by,error_message,timeoutInSecond);
-        element.click();
-        return element;
-    }
-
-    private WebElement waitForElementAndSendKeys(By by, String value, String error_message, long timeoutInSecond){
-
-        WebElement element = waitForElementPresent(by,error_message,timeoutInSecond);
-        element.sendKeys(value);
-        return element;
-    }
-
-    private String searchTitleWithoutTimeout(By by, String attribute, String error_message){
-
-        WebElement element = waitForElementPresent(by,error_message,0);
-        return element.getAttribute(attribute);
-    }
 
 }

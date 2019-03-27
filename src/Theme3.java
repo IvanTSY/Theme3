230a6f23cdaff58e9a7fc9d6c0ceec59c664fd90
IvanTsypyshev <deadline.3012@gmail.com>
2019-03-27 22:11:14 +0700

    Test6

--- src/Theme3.java
+++ src/Theme3.java
@@ -1,20 +1,16 @@
 import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
-import java.util.List;
 
 public class Theme3 {
     private AppiumDriver driver;
@@ -43,17 +39,20 @@ public class Theme3 {
     public void tearDawn(){
         driver.quit();
     }
+    String search_line = "Appium";
 
     @Test
 
     public void findTitleWithoutTimeOut() {
+
+
 
         waitForElementAndClick(
                 By.xpath("//*[contains(@text,'Search Wikipedia')]"),
                 "Can not Find Search Wikipedia",
                 5);
 
-        String search_line = "Appium";
+
 
         waitForElementAndSendKeys(
                 By.xpath("//*[contains(@text,'Search…')]"),
@@ -64,20 +63,20 @@ public class Theme3 {
         waitForElementAndClick(
                 By.xpath("//*[@resource-id = 'org.wikipedia:id/page_list_item_container']//*[@text='Appium']"),
                 "Can not Find Search Wikipedia topic searching by" + search_line,
-                5);
+                15);
 
 
-                String title_before = waitForElementAndGetAttribute(
+        String attribForSearch  = searchTitleWithoutTimeout(
                 By.id("org.wikipedia:id/view_page_title_text"),
                 "text",
-                "Cannot find title of article",
-                0
+                "Cannot find title of article"
         );
 
         Assert.assertEquals(
-                "Article title not equals the java title",
-                "Appium",
-                title_before
+                "Article title not equals the "+search_line+" title",
+                search_line,
+                attribForSearch
+
         );
 
 
@@ -107,22 +106,10 @@ public class Theme3 {
         return element;
     }
 
-    private String waitForElementAndGetAttribute(By by, String attribute, String error_message, long timeOutinSeconds){
-        WebElement element = waitForElementPresent(by,error_message,timeOutinSeconds);
+    private String searchTitleWithoutTimeout(By by, String attribute, String error_message){
+
+        WebElement element = waitForElementPresent(by,error_message,0);
         return element.getAttribute(attribute);
     }
-
-
-//    private String GetAttribute(By by, String attribute, String error_message){
-//        WebElement element = ElementPresent(by,error_message);
-//        return element.getAttribute(attribute);
-//    }
-//
-//    private WebElement ElementPresent(By by, String error_message){
-//
-//        return ElementPresent(by,error_message);
-//
-//    }
-
 
 }
